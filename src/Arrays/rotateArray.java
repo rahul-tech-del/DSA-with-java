ae51cae00fb39e66c2390f4d506dc5e52f172dd2
rahul-tech-del <patelr2294@gmail.com>
2026-07-31 00:54:37 +0530

    Arrays

--- src/Arrays/rotateArray.java
+++ src/Arrays/rotateArray.java
@@ -1,29 +1,26 @@
 package Arrays;
-
+import java.util.Arrays;
 public class rotateArray {
-    public static void main(String[] args) {
-        int[] arr = {10,20,30,40,50,60,70,80};
-        int n = arr.length;
-
-        for(int ele : arr){
-            System.out.print(ele+" ");
-        }
-        System.out.println();
-        int i=2 , j=5;
+    public void reverse(int[] arr, int i,int j){
         while(i<=j){
-            swap( arr,i,j);
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
             i++;
             j--;
         }
-        for(int ele : arr){
-            System.out.print(ele+" ");
-        }
-          System.out.println();
-    }  
-    public static void swap(int[] arr, int i , int j){
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
     }
-    
+    public void rotate(int[] arr, int k){
+        int n = arr.length;
+        k = k % n;
+        reverse(arr,0,n-k-1);
+        reverse(arr,0,n-1);
+    }
+    public static void main(String[] args) {
+        rotateArray obj = new rotateArray();
+        int[] arr = {10,20,30,40,50,60,70,80};
+        int k = 3;
+        obj.rotate(arr , k);
+        System.out.println(Arrays.toString(arr));
+    }
 }
