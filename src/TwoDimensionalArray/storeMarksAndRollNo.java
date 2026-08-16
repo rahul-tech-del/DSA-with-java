93b8726ccb9d21e6b75eb601b2f93d0066ff034f
rahul-tech-del <patelr2294@gmail.com>
2026-08-16 23:27:20 +0530

    storeMarksAndRollNo

---
+++ src/TwoDimensionalArray/storeMarksAndRollNo.java
@@ -0,0 +1,23 @@
+package TwoDimensionalArray;
+
+import java.util.Scanner;
+
+public class storeMarksAndRollNo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] arr = new int[4][2];
+        int n = arr.length;
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 2; j++) {
+              arr[i][j] = sc.nextInt();    
+            }           
+        }
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print(arr[i][j]+" ");            
+            }
+            System.out.println();
+        }
+    }
+}
