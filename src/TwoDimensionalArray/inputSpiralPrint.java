2fc88e6c4f0125cec18be4d340f2f958c80d12f0
rahul-tech-del <patelr2294@gmail.com>
2026-08-26 01:34:06 +0530

    inputSpiralPrint

---
+++ src/TwoDimensionalArray/inputSpiralPrint.java
@@ -0,0 +1,45 @@
+package TwoDimensionalArray;
+
+import java.util.Scanner;
+
+public class inputSpiralPrint {
+    public static void print(int[][] arr){
+        int m = arr.length, n = arr[0].length;
+        for(int i=0; i<m; i++){
+            for(int j=0; i<n; j++){
+                System.out.print(arr[i][j]+" ");
+            }
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int[][] arr = new int[m][n];
+        int minr = 0, maxr = m-1;
+        int minc = 0, maxc = n-1;
+
+        while(minr<=maxr && minc<=maxc){
+            // left to right
+            for(int j=minc;j<=maxc;j++){
+                System.out.print(arr[minr][j]+" ");
+            }
+            minr++;
+            // top to bottom
+            if(minr>maxr || minc>maxc) break;
+            for(int i=minr; i<=maxr; i++){
+                System.out.print(arr[i][maxc]+" ");
+            }maxc--;
+            // right to left 
+            if(minr>maxr || minc>maxc) break;
+            for(int j=maxc; j>=minc;j--){
+                System.out.print(arr[maxr][j]+" ");
+            }maxr--;
+            if(minr>maxr || minc>maxc) break;
+            for(int i=maxr; i>=minr; i--){
+                System.out.print(arr[i][minc]+" ");
+            }minc++;
+        }
+    }
+}
