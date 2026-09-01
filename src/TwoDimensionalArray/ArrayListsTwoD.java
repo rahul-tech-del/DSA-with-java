6afbb1d80b0ada74a787200288a54502c2503d66
rahul-tech-del <patelr2294@gmail.com>
2026-09-02 00:36:44 +0530

    TwoDimensionalArray

---
+++ src/TwoDimensionalArray/ArrayListsTwoD.java
@@ -0,0 +1,24 @@
+package TwoDimensionalArray;
+
+import java.util.*;
+
+public class ArrayListsTwoD {
+    public static void main(String[] args) {
+        List<Integer> a = new ArrayList<>();
+        a.add(10); a.add(20); a.add(30);
+        List<Integer> b = new ArrayList<>();
+        b.add(40); b.add(50);
+        List<Integer> c = new ArrayList<>();
+        List<Integer> d = new ArrayList<>();
+        d.add(60);
+        List<List<Integer>> l = new ArrayList<>();
+        l.add(a); l.add(b); l.add(c); l.add(d);
+
+        for(int i=0; i<l.size(); i++){
+            for(int j=0; j<l.get(i).size();j++){
+                System.out.print(l.get(i).get(j)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
