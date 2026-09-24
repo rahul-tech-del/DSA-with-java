f51d0f85b1fe749c20f99338fb397ed13c4cdcac
rahul-tech-del <patelr2294@gmail.com>
2026-09-24 23:57:36 +0530

    setMatrixZeroes

---
+++ src/TwoDimensionalArray/setMatrixZeroes.java
@@ -0,0 +1,59 @@
+package TwoDimensionalArray;
+
+import java.util.Scanner;
+
+public class setMatrixZeroes {
+    public static void main(String[] args) {
+           Scanner sc = new Scanner(System.in);
+
+        // Rows and columns input
+        System.out.print("Enter rows: ");
+        int n = sc.nextInt();
+
+        System.out.print("Enter columns: ");
+        int m = sc.nextInt();
+
+        int[][] matrix = new int[n][m];
+
+        // Matrix input
+        System.out.println("Enter matrix elements:");
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        // Find zeroes
+        boolean[] row = new boolean[n];
+        boolean[] col = new boolean[m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+        }
+
+        // Set zeroes
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (row[i] || col[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Print matrix
+        System.out.println("Output:");
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
